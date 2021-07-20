c6fdacda688610cd591c4055d8a441fe033c0f6d
vladislav <perets1995@gmail.com>
2021-07-20 15:41:16 +0300

    next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../styles/globals.css'
+import Head from 'next/dist/next-server/lib/head'
+import React from 'react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return (
+		<>
+			<Head>
+				<title>MyTop</title>
+				<link rel="icon" href="/fovicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
