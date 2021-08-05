3f779db36c13bf58b4466af0001f0a8cb468e84a
vladislav <perets1995@gmail.com>
2021-08-05 19:37:43 +0300

    create build header

--- components/hader/header.tsx
+++ components/hader/header.tsx
@@ -3,10 +3,17 @@ import React from "react"
 export const Header = ():JSX.Element => {
 	return(
 		<header >
-			<a href=""><img src="/header/logoLoveBeauty.png" alt="" width='36px'/>
+			<a className='center' href="https://www.figma.com/file/bR8kVVEBmaECwixS25mRgo/Update_Touch%C3%A9?node-id=817%3A19481"><img src="/header/logoLoveBeauty.png" alt="" width='36px'/>
 				love&Beauty
 			</a>
-			
+			<nav className='center'>
+				<a href="#" className='revie'>обзор</a>
+				<a href="#" className='shop'>магазин</a>
+				<a href="#" className='surprise'>подарки</a>
+			</nav>
+			<a href="" className='center'><div className="userName">vladislavskiy</div><img src="/header/LK.svg" alt="" /></a>
+			<a href=""><img src="/header/sell.svg" alt="" /></a>
+			<a href=""><img src="/header/search.svg" alt="" /></a>
 		</header>
 	)
 }
