d81c96e04801387e50a9527d77736d6f1ff008ae
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2021-08-05 10:45:24 +0300

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import Head from 'next/dist/next-server/lib/head'
+import React from 'react'
+import '../css/global.css'
+
+ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>Love&Beauty</title>
+        <link key={0} rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps}/>
+    </>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Love&Beauty</title>
+        <link key={0} rel="icon" href="/favicon.ico" />
+      </Head>
+
+			<header className='header'>
+				hello
+			</header>
+
+      <main className='main'>
+
+      </main>
+
+      <footer className='footer'>
+      </footer>
+    </>
+  )
+}
