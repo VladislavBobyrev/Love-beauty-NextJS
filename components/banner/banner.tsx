b27ed351d7366f224cfba41522d09e7597d1e1e9
vladislav <perets1995@gmail.com>
2021-08-09 16:42:33 +0300

    sliger

---
+++ components/banner/banner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SliderData } from './sliderData';
+
+
+export const Banner = ():JSX.Element => {
+  	return(	
+		<div>
+			<li><a href=""><img src='/banner/cover.jpg' alt="" /></a></li>
+			<li><a href=""><img src='/banner/lip.jpg' alt="" /></a></li>
+			<li><a href=""><img src='/banner/cover.jpg' alt="" /></a></li>
+			<li><a href=""><img src='/banner/lip.jpg' alt="" /></a></li>
+		</div>
+	)
+}
+
--- components/main/main.tsx
+++ components/main/main.tsx
@@ -1,7 +1,10 @@
 import React from "react"
+import { Banner } from "../banner/banner"
 
 export const Main = ():JSX.Element => {
 	return(
-		<></>
+		<main>
+			<Banner/>
+		</main>
 	)
 }
